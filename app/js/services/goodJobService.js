d13309a081aa966bb4e4fa60e1ea2017cc864d10
Joakim Larsson <joakimlarsson92@gmail.com>
2015-03-15 18:07:46 +0100

    Added functionallity and functionlist
    Added this functionallity to GoodJobProvider:
    - getApplicationLogo (Not fully implemented): Get logo of application,
    haven't tried out fully yet
    - analyzeApplicationText: Get keywords from text, could be used for the
    applicationtext later on
    
    Also started working on a functionlist presented in the goodJobService
    with comments

--- app/js/services/goodJobService.js
+++ app/js/services/goodJobService.js
@@ -8,10 +8,85 @@ goodJobApp.factory('GoodJob', function ($resource, $routeParams){
 	this.getApplication = $resource("http://api.arbetsformedlingen.se/platsannons/:annonsId",{},{
 		get:{
             method:"GET",
-            headers:{'Content-Type':'application/json;charset=UTF-8', 'Accept':'application/json'} 
+            headers:{'Content-Type':'application/json;charset=UTF-8', 'Accept':'application/json', 'Accept-Language':'sv-SE,sv'} 
         }
 	});
+			
+	this.getApplicationLogo = $resource("http://api.arbetsformedlingen.se/platsannons/:annonsId/logotyp", {},{
+		get:{
+			method:"GET",
+			headers:{'Content-Type':'images/gif', 'Accept-Language':'sv-SE,sv'}
+		}
+	});
+
+	//Method for getting keywords from application text, maybe useful?
+	this.analyzeApplicationText = $resource("http://access.alchemyapi.com/calls/text/TextGetRankedKeywords", {apikey:'cafd0c37b7a9d2c79519455c289e3658276efc81', outputMode:'json', maxRetrieve:3});
+
 	return this;
+
+
+});
+
+
+/* Contains of all personal information of the users, should use firebase integration for security of storing myProfile, newJobs and myApplications */
+goodJobApp.factory('PersonalProfile', function(){
+		var newJobs = [];
+		var myApplications = [];
+		var myProfile = [];
+
+		//-------------- PROFILE INFO ------------------------
+
+		//getting profile info
+		this.getAllProfileInfo = function(){
+			return = myProfile;
+		}
+
+		//Updates single post in profile
+		this.updateInfoInProfile = function(indexToUpdate){
+
+		}
+
+		//Maybe not necessary?
+		this.removeInfoFromProfile = function(indexToRemove){
+
+		}
+
+		//------------- NEW JOBS -----------------------------
+
+		//Getting available jobs
+		this.getNewJobs = function(){
+			return newjobs;
+		}
+
+		this.addNewJobToProfile = function(){
+
+		}
+
+		this.removeNewJobFromProfile = function(){
+
+		}
+
+		// ------------My applications ----------------------
+		
+		/*Add application to my applications
+			IN: applicationToAdd - application to add to myApplications
+		*/
+		this.addNewApplication = function(applicationToAdd){
+
+		}
+		/*Remove application from my applications
+			IN: id - applicationid of application to remove from myApplications
+		*/
+		this.removeApplication = function(id){
+
+		}
+
+		//Returns all searched applications 
+		this.getAllApplications = function(){
+			return myApplications;
+		}
+
+
 });
 
 //Maybe used later for login authentication
