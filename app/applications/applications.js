93f6aed21653447f0494f0a9678fb0f6b8dbc52b
Trulls <annaj9@kth.se>
2015-03-31 15:07:59 +0200

    Added test active applications

--- app/applications/applications.js
+++ app/applications/applications.js
@@ -21,6 +21,7 @@ angular.module('goodJob.applications', ['firebase.auth', 'firebase.utils', 'ngRo
 
 	.controller("ApplicationsCtrl", ["$scope", "Auth", "$location",
 		function($scope, Auth, $location) {				
+<<<<<<< HEAD
 >>>>>>> anna_branch
 
         $scope.ads =[{  company_name:     "Bison",
@@ -65,3 +66,39 @@ angular.module('goodJob.applications', ['firebase.auth', 'firebase.utils', 'ngRo
 
 	}]);
 >>>>>>> anna_branch
+=======
+
+        $scope.ads =[{  company_name:     "Bison",
+                        company_match:    "76%",
+                        company_logo:     "https://cdn.tutsplus.com/vector/uploads/legacy/articles/linkb_20weirdlogos/weirdlogos_prev.jpg",
+                          job_title:      "Meat taster",
+                          job_hours:      "Part time, 25%",
+                          job_city:       "Madrid",
+                          job_starts:     "May 2015",
+                          job_ends:       "May 2016",
+                          job_deadline:   "2015-04-01",
+                          job_status:     "Accepted" 
+                      },{ company_name:   "Ballet Acadamy",
+                        company_match:    "85%",
+                        company_logo:     "http://www.virginiabeachballetacademy.com/rw_common/images/VBBA%20Logo%20120H.png",
+                          job_title:      "Shoe maker",
+                          job_hours:      "Full time, 100%",
+                          job_city:       "Malmö",
+                          job_starts:     "May 2015",
+                          job_ends:       "Dec 2016",
+                          job_deadline:   "2015-03-31",
+                          job_status:     "Accepted" 
+                      }]
+
+        $scope.continue = function(value) {
+          console.log(value);
+          if (value === 'Accepted') {
+            alert("Congratulations! Please meet us in Mars 2080 for your interview!");
+          }
+          else {
+            alert("You need to get your application accepted first! Please be patient...");
+          }
+        }
+
+	}]);
+>>>>>>> Added test active applications
