447184ce87cf341fb79e56ac950ffcdeb923055d
Joakim Larsson <joakimlarsson92@gmail.com>
2015-04-15 23:23:10 +0200

    Fixed login/logout bug
    Changed the use of setUser(). Instead of using onAuth we are now using getAuth which gets the Authentication once.

--- app/components/applicationAPI/applicationAPI.js
+++ app/components/applicationAPI/applicationAPI.js
@@ -7,13 +7,13 @@ angular.module('applicationAPI', ['ngResource'])
             headers:{'Content-Type':'application/json;charset=UTF-8', 'Accept':'application/json', 'Accept-Language':'sv-SE,sv'} 
         }
   });
-  	//Method for getting single application from Arbetsformedlingen API
-	this.getApplication = $resource("http://api.arbetsformedlingen.se/platsannons/:annonsId",{},{
-		get:{
+    //Method for getting single application from Arbetsformedlingen API
+  this.getApplication = $resource("http://api.arbetsformedlingen.se/platsannons/:annonsId",{},{
+    get:{
             method:"GET",
             headers:{'Content-Type':'application/json;charset=UTF-8', 'Accept':'application/json', 'Accept-Language':'sv-SE,sv'} 
         }
-	});
+  });
 
     return this;
   }]);
