ed77f86de3be9899862835adf165241ce9850ca4
Joakim Larsson <joakimlarsson92@gmail.com>
2015-04-16 00:48:08 +0200

    Fixed apply
    It was some bug with getting the request for a single application. Solved it by adding id as parameter to request. Now everything is synced correctly I think.

--- app/apply/apply.js
+++ app/apply/apply.js
@@ -13,24 +13,25 @@ angular.module('goodJob.apply', ['firebase.auth', 'firebase.utils', 'ngRoute'])
 
 			console.log("Apply for jobID: " + $routeParams.jobID);
 
-	    ApplicationAPI.getApplication.get({annonsID: $routeParams.jobID}, function (data) {
+	    ApplicationAPI.getApplication.get({'Id': $routeParams.jobID}, function (data) {
 	        console.log("Response from ApplicationAPI.getAppliction:", data);
+	        var platsannons = data.platsannons;
 	        $scope.ad = {
-	            company_name: matchedJobs[i].arbetsplatsnamn,
-							company_logo: "http://vignette3.wikia.nocookie.net/disney-infinity/images/7/75/Monsters_Inc.jpg/revision/latest?cb=20130118180017",
-							job_header: matchedJobs[i].annonsrubrik,
-							job_id: matchedJobs[i].annonsid,
-							job_title: matchedJobs[i].yrkesbenamning,
-							job_city: matchedJobs[i].kommunnamn,
-							job_address: matchedJobs[i].arbetsplats.postadress + ", " + matchedJobs[i].arbetsplats.postnummer + " " + matchedJobs[i].arbetsplats.postort,
-							job_positions: matchedJobs[i].antal_platser,
-							job_conditions: matchedJobs[i].villkor.varaktighet,
-							job_hours: matchedJobs[i].villkor.arbetstid,
-							job_link: matchedJobs[i].platsannonsUrl,
-							job_posted: matchedJobs[i].publiceraddatum.substring(0,10),
-							job_deadline: matchedJobs[i].ansokan.sista_ansokningsdag.substring(0,10),
-							job_description: matchedJobs[i].annonstext.substring(0,50) + " ...",
-							job_competences: ["Excel","Word","Paragliding"]
+	            company_name: "",
+				company_logo: "http://vignette3.wikia.nocookie.net/disney-infinity/images/7/75/Monsters_Inc.jpg/revision/latest?cb=20130118180017",
+				job_header: platsannons.annons.annonsrubrik,
+				job_id: platsannons.annons.annonsid,
+				job_title: platsannons.annons.yrkesbenamning,
+				job_city: platsannons.annons.kommunnamn,
+				job_address: platsannons.arbetsplats.postadress + ", " + platsannons.arbetsplats.postnummer + " " + platsannons.arbetsplats.postort,
+				job_positions: platsannons.antal_platser,
+				job_conditions: platsannons.villkor.varaktighet,
+				job_hours: platsannons.villkor.arbetstid,
+				job_link: platsannons.platsannonsUrl,
+				job_posted: platsannons.annons.publiceraddatum.substring(0,10),
+				job_deadline: platsannons.ansokan.sista_ansokningsdag.substring(0,10),
+				job_description: platsannons.annons.annonstext.substring(0,50) + " ...",
+				job_competences: ["Excel","Word","Paragliding"]
 	        }
 	    }, function (data) {
 	        console.log("There was an error");
