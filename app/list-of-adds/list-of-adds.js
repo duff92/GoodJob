cd2c53d74440c4f53cb4c63a46de8171e04890b1
Trulls <annaj9@kth.se>
2015-04-15 22:50:29 +0200

    List-of-adds now shows the 10 latest available jobs from Arbetsförmedlingen, if so with only basic information and the same logo

--- app/list-of-adds/list-of-adds.js
+++ app/list-of-adds/list-of-adds.js
@@ -10,49 +10,32 @@ angular.module('goodJob.listOfAdds', ['firebase.auth', 'firebase.utils', 'ngRout
 
 	}])
 
-	.controller("ApplicationCtrl", ["$scope", "$routeParams", "ApplicationAPI",
-		function ($scope, $routeParams, ApplicationAPI) {
+	.controller("ApplicationCtrl", ["$scope", "$routeParams", "$location", "ApplicationAPI",
+		function ($scope, $routeParams, $location, ApplicationAPI) {
 
 
 		    ApplicationAPI.latestApplications.get(function (data) {
 		        console.log("Response from ApplicationAPI.latestApplictions:", data);
-		        $scope.matchedJobs = data.matchningslista.matchningdata;
-		        console.log("Mathcningdata", $scope.matchedJobs);
-		        
-		        $scope.ads = [{
-		            company_name: matchedJobs[0].arbetsplatsnamn,
-		            company_logo: "http://vignette3.wikia.nocookie.net/disney-infinity/images/7/75/Monsters_Inc.jpg/revision/latest?cb=20130118180017",
-		            job_id: matchedJobs[0].annonsid,
-		            job_title: matchedJobs[0].annonsrubrik,
-		            job_hours: "Part time, 50%",
-		            job_city: "Stockholm",
-		            job_starts: "May 2015",
-		            job_ends: "May 2016",
-		            job_deadline: "2015-04-15"
-		        }, {
-		            company_name: "Sunshine Caramel co",
-		            company_logo: "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-frc3/v/t1.0-1/p160x160/1176357_376072752522448_1055231562_n.jpg?oh=a37afcd1f8a8d417ee2acdd479f95ee5&oe=55BC8F21&__gda__=1433796570_24e35ba39dace0da18cf7a186181f43a",
-		            job_id: "2",
-		            job_title: "Cola maker",
-		            job_hours: "Full time, 100%",
-		            job_city: "Uppsala",
-		            job_starts: "May 2015",
-		            job_ends: "Dec 2016",
-		            job_deadline: "2015-03-31"
-		        }, {
-		            company_name: "Dinosaur train",
-		            company_logo: "http://anagramballoons.com/CMSPages/Getfile.aspx?guid=be25fda0-1556-4c0d-b466-7c58c9b60a5e",
-		            job_id: "3",
-		            job_title: "Train driver",
-		            job_hours: "Part time, 75%",
-		            job_city: "Stockholm",
-		            job_starts: "Jun 2015",
-		            job_ends: "Dec 2016",
-		            job_deadline: "2015-04-30"
-		        }]
+		        var matchedJobs = data.matchningslista.matchningdata;
+		        console.log("Mathcningdata", matchedJobs);
+
+		        $scope.ads = [];
+
+		        for (var i = 0; i < matchedJobs.length; i++) {
+		        	$scope.ads.push({
+						company_name: matchedJobs[i].arbetsplatsnamn,
+						company_logo: "http://vignette3.wikia.nocookie.net/disney-infinity/images/7/75/Monsters_Inc.jpg/revision/latest?cb=20130118180017",
+						job_header: matchedJobs[i].annonsrubrik,
+						job_id: matchedJobs[i].annonsid,
+						job_title: matchedJobs[i].yrkesbenamning,
+						job_city: matchedJobs[i].kommunnamn,
+						job_posted: matchedJobs[i].publiceraddatum.substring(0,10),
+		        	})
+		        };
+
 		    }, function (data) {
 		        console.log("There was an error");
-		        alert("There was an error loading the data");
+		        //alert("There was an error loading the data");
 		    });
 
 			$scope.applyForJob = function (id) {
